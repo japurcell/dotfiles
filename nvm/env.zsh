c13b19ae5dc601ff762f83728a5f00454492ce12
japurcell <japurcell@users.noreply.github.com>
2020-06-28 14:19:36 -0700

    Adding dry-run and merge options

--- nvm/env.zsh
+++ nvm/env.zsh
@@ -1,4 +1,5 @@
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] \
-    && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+if [[ ! -z "$NVM_DIR" ]] && [[ -d "$NVM_DIR" ]]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi

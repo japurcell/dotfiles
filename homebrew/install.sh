b75de41d67eeacaf4aee96e5e22cca0fb4d2a2dc
japurcell <japurcell@users.noreply.github.com>
2020-06-27 19:10:05 -0700

    Making scripts more linux friendly

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,18 +5,14 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+if [ "$(uname -s)" = "Darwin" ]; then
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
+  # Check for Homebrew
+  if test ! $(which brew)
   then
+    echo "  Installing Homebrew for you."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+
   fi
 
 fi

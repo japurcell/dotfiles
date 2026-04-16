ed6d3b3a289d8ba644a0264b50f6c185d7c34f3c
japurcell <japurcell@users.noreply.github.com>
2026-04-16 14:19:14 -0700

    Add OpenSpec completions and update environment variables in zsh configuration

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
-
+alias ec='vscode ~/.dotfiles/zsh/aliases.zsh'
 alias cls='clear' # Good 'ol Clear Screen command
+alias cp:y='copilot --experimental --yolo --effort high'
+alias vscode='~/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
+alias vscode:settings='vscode ~/Library/Application\ Support/Code/User/settings.json'

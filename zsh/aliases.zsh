1a7bb608e74638b13ed96acfd0a1165ed7e543bb
japurcell <japurcell@users.noreply.github.com>
2026-04-16 14:21:08 -0700

    Update copilot alias to specify model version

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias ec='vscode ~/.dotfiles/zsh/aliases.zsh'
 alias cls='clear' # Good 'ol Clear Screen command
-alias cp:y='copilot --experimental --yolo --effort high'
+alias cp:y='copilot --experimental --yolo --effort high --model gpt-5.4-mini'
 alias vscode='~/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
 alias vscode:settings='vscode ~/Library/Application\ Support/Code/User/settings.json'

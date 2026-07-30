2066b29d3294d6c97d473df2701f3b7c7bce120e
Adam Purcell <japurcell@users.noreply.github.com>
2026-07-30 07:05:19 -0700

    Add alias for updating dotfiles with git pull and bootstrap script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,4 @@ alias reload!='. ~/.zshrc'
 alias ec='vscode ~/.dotfiles/zsh/aliases.zsh'
 alias cls='clear' # Good 'ol Clear Screen command
 alias cp:y='copilot --experimental --yolo --effort high --model gpt-5.4-mini'
+alias dotfiles:update='(cd ~/.dotfiles && git pull && ./script/bootstrap)'
